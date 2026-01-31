048fc3a6622010f868c84c35a373a3d65c1fa106
joel sorel <toya@info.com>
2026-01-31 12:18:00 +0100

    correction5

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -73,10 +73,10 @@ const Hero = () => {
                         </div>
                         <div className="feature-text">
                             <h4>Écosystème de Réussite</h4>
-                            <p>Web Entrepreneurs™ allie innovation et accompagnement pour mettre à votre disposition une structure clé en main dédiée à la vente de services digitaux.</p>
+                            <p>Ditall Global™ allie innovation et accompagnement pour mettre à votre disposition une structure clé en main dédiée à la vente de services digitaux.</p>
                         </div>
                     </div>
-                    <div className="feature-box orange">
+                    <div className="featuqre-box orange">
                         <div className="feature-icon-wrap">
                             <Zap size={24} />
                         </div>
